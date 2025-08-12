9eaec6b3194dca2c2fa53ee983993036173e1f2e
Caleb <caleb_keenan@paraparaumucollege.school.nz>
2025-08-13 11:32:15 +1200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 
 project = "pyrend"
-html_title = "PyRend 0.1.31 Documentation"
+html_title = "PyRend 0.1.40 Documentation"
 html_logo = "_static/favicon.png"
 author = "CalebK33"
 release = "0.1.3"
