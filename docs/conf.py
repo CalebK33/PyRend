70c397fd135582a15afb7e4d8bbda825cec64617
Caleb <caleb_keenan@paraparaumucollege.school.nz>
2025-08-25 09:51:13 +1200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,10 +3,10 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 
 project = "pyrend"
-html_title = "PyRend 0.1.47 Documentation"
+html_title = "PyRend 0.1.48 Documentation"
 html_logo = "_static/favicon.png"
 author = "CalebK33"
-release = "0.1.47"
+release = "0.1.48"
 
 extensions = [
     "sphinx.ext.autodoc",
